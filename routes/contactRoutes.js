b4b15b2ff5629418e5997ea32ca31709adfe0d5d
Bharath799 <bharathgbd1@gmail.com>
2025-05-03 17:43:24 +0530

    http methods implemented

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -4,5 +4,16 @@ const router = express.Router();
 router.route("/").get((req,res) => {
     res.status(200).json({message:"Get all contacts"})
 });
-
+router.route("/:id").get((req,res) => {
+    res.status(200).json({message:`Get all contacts ${req.params.id}`})
+});
+router.route("/").post((req,res) => {
+    res.status(200).json({message:`create contacts`})
+});
+router.route("/:id").put((req,res) => {
+    res.status(200).json({message:`Update contact for ${req.params.id}`})
+});
+router.route("/:id").delete((req,res) => {
+    res.status(200).json({message:`Delete contacts ${req.params.id}`})
+});
 module.exports = router
