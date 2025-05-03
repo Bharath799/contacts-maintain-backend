c8a46cd941c8229de72c921a2f3b2c2c74cf4531
Bharath799 <bharathgbd1@gmail.com>
2025-05-03 10:58:28 +0530

    routes update

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 //Create basic express server npm install express
 //Install dontenv
 //install thunder client
+//to handle proper routes use Routes folder structure
+//app.use() is a middle ware
 
 const express = require("express");
 const env = require("dotenv").config();
@@ -8,6 +10,12 @@ const env = require("dotenv").config();
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+// app.get('/api/contacts',(req,res) => {
+//     res.send({message:"Get Contacts"})
+// })
+//middle ware
+app.use("/app/contacts",require("./routes/contactRoutes"));
 
 app.listen(port,() =>{
     console.log("server started on :",port); 
