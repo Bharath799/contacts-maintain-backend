3d743b1b38c2f3f4db4841bd47ffa8207c463f50
Bharath799 <bharathgbd1@gmail.com>
2025-05-03 09:03:07 +0530

    server-setep-done

---
+++ server.js
@@ -0,0 +1,11 @@
+//Create basic express server npm install express
+
+const express = require("express");
+
+const app = express();
+
+const port = 5000;
+
+app.listen(port,() =>{
+    console.log("server started on :",port); 
+})
