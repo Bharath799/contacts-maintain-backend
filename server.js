84e37708a95a165efd9ca172eee58b81ee52c7d8
Bharath799 <bharathgbd1@gmail.com>
2025-05-03 10:08:21 +0530

    env update

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 //Create basic express server npm install express
+//Install dontenv
+//install thunder client
 
 const express = require("express");
+const env = require("dotenv").config();
 
 const app = express();
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port,() =>{
     console.log("server started on :",port); 
