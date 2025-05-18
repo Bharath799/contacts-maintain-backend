efe6b67d2e656ac542c6b3d170651f711e447451
Bharath799 <bharathgbd1@gmail.com>
2025-05-18 20:05:50 +0530

    upto get contacts

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,50 +1,56 @@
 //mongoDB always get promise to resolve it we need add try catch block in each function of async
 //instead of async we are using express middleware 'express-async-handler' it handle the exceptions in side of async express routes
 //so we dont have to write all the try catch block in order to catch errors 'asyncHandler' will automatically call errorHandler when exception occur
-const asyncHandler = require("express-async-handler")
+const asyncHandler = require("express-async-handler");
+const Contacts = require("../models/contactModel");
 
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access public
 
-const getContacts = asyncHandler( async (req,res) => {
-    res.status(200).json({message:"Get all contacts"})
-})
+const getContacts = asyncHandler(async (req, res) => {
+  const contacts = await Contacts.find();
+  res.status(200).json({ message: contacts });
+});
 
 //@desc Create contacts
 //@route PUT /api/contacts
 //@access public
 
-const getContact = asyncHandler(async (req,res) => {
-    res.status(200).json({message:`Get contact ${req.params.id}`})
-})
+const getContact = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Get contact ${req.params.id}` });
+});
 
-const createContacts = asyncHandler(async (req,res) => {
-    console.log("Request Recived Hello",req.body);
-    const {name,Address,Contact} = req.body;
-    if(!name || !Address || !Contact){
-        res.status(400)
-        throw new Error("All Fields Mandatory")
-    }
-    res.status(200).json({message:req.body})
-
-})
+const createContacts = asyncHandler(async (req, res) => {
+  console.log("Request Recived Hello", req.body);
+  const { name, Address, Contact } = req.body;
+  if (!name || !Address || !Contact) {
+    res.status(400);
+    throw new Error("All Fields Mandatory");
+  }
+  res.status(200).json({ message: req.body });
+});
 
 //@desc Delete contacts
 //@route DELETE /api/contacts
 //@access public
 
-const deleteContact = asyncHandler(async (req,res) => {
-    res.status(200).json({message:`Delete contacts ${req.params.id}`})
-})
+const deleteContact = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Delete contacts ${req.params.id}` });
+});
 
 //@desc Create contacts
 //@route PUT /api/contacts
 //@access public
 
-const updateContact = (req,res) => {
-    res.status(200).json({message:`Update contact for ${req.params.id}`})
-}
+const updateContact = (req, res) => {
+  res.status(200).json({ message: `Update contact for ${req.params.id}` });
+};
 
-
-module.exports = {getContacts,getContact,createContacts,deleteContact,updateContact}
+module.exports = {
+  getContacts,
+  getContact,
+  createContacts,
+  deleteContact,
+  updateContact,
+};
